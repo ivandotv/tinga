78c3aeb936e3425270ffa87bc4ab757b1dc48929
ivandotv <390700+ivandotv@users.noreply.github.com>
2022-03-29 22:37:35 +0200

    chore: index file cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,2 @@
-//TODO - log levels
-//TODO - everything else
-//TODO - send beacon
-//TODO - pass in function as first argument, -> get context injetect
-//TODO - set context
-//TODO - get context
-//TODO - child override - create
-//TODO - get - set level
-//TODo - get set beacon level
-//TODO - label
-//TODO - add label - namespace
-
 export * from './types'
 export * from './minilog'
-
-// const namespaceColors: {
-//   [key in LogLevelName]: { color: string }
-// } = {
-//   debug: { color: '#8367d3' },
-//   error: { color: '#ff1a1a' },
-//   fatal: { color: '#ff1a1a' },
-//   info: { color: '#3291ff' },
-//   trace: { color: '#999' },
-//   warn: { color: '#f7b955' }
-// }
